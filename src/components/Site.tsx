956969203315c66e5f9676f68b761ddea63efc92
odinokun <odinokun@gmail.com>
2024-12-12 19:24:16 -0500

    add routing and initial page components for the application

---
+++ src/components/Site.tsx
@@ -0,0 +1,15 @@
+import styles from './Site.module.css';
+
+export const Site = () => {
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>1234</div>
+        <div className={styles.content}>content</div>
+      </div>
+    </div>
+  );
+};
