f83d9ac5825c4ac2ffca07d52f4b11d77f2834a6
odinokun <odinokun@gmail.com>
2024-12-13 12:36:54 -0500

    add admin warning for specific page in Page component

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { PageType } from '../../data/dataState';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import { Error404 } from './Error404';
 
 type PropsType = {
@@ -11,8 +11,11 @@ export const Page: FC<PropsType> = ({ pages }) => {
   const params = useParams();
   const page = params.link ? pages[+params.link] : null;
 
+  const location = useLocation();
+
   return page ? (
     <div>
+      {location.pathname === '/page/1' && <h2>Only for admin!!!</h2>}
       <h1>{page.heading}</h1>
       <img src={page.image} alt={page.heading} />
       <p>{page.about}</p>
