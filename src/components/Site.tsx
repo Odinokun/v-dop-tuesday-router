f9f9f26c7f2dd91d43070cc5498669948205e66e
odinokun <odinokun@gmail.com>
2024-12-12 21:18:27 -0500

    add styled-components for navigation styling and refactor Site component

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -4,6 +4,7 @@ import { PageThree } from './pages/PageThree';
 import { PageTwo } from './pages/PageTwo';
 import { Error404 } from './pages/Error404';
 import styles from './Site.module.css';
+import styled from 'styled-components';
 
 export const Site = () => {
   return (
@@ -14,34 +15,25 @@ export const Site = () => {
 
       <div className={styles.body}>
         <div className={styles.nav}>
-          <nav>
-            <NavLink
-              to='/page-1'
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-              end
-            >
-              Adidas
-            </NavLink>
-            <NavLink
-              to='/page-2'
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-              end
-            >
-              Puma
-            </NavLink>
-            <NavLink
-              to='/page-3'
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-              end
-            >
-              Nike
-            </NavLink>
+          <nav className={NavWrapper}>
+            {/* <NavLink to='/page-1' className={({ isActive }) => isActive ? styles.active : styles.navLink} end >Test</NavLink> */}
+            <NavWrapper>
+              <NavLink to='/page-1' end>
+                Adidas
+              </NavLink>
+            </NavWrapper>
+
+            <NavWrapper>
+              <NavLink to='/page-2' end>
+                Puma
+              </NavLink>
+            </NavWrapper>
+
+            <NavWrapper>
+              <NavLink to='/page-3' end>
+                Nike
+              </NavLink>
+            </NavWrapper>
           </nav>
         </div>
         <div className={styles.content}>
@@ -59,3 +51,24 @@ export const Site = () => {
     </div>
   );
 };
+
+const NavWrapper = styled.nav`
+  margin-left: 10px;
+  font-size: 20px;
+  padding: 10px;
+  & > a {
+    padding: 5px;
+    text-decoration: none;
+    color: chocolate;
+    transition: all 0.3s;
+  }
+
+  & > a.active {
+    color: #067539;
+    text-decoration: dashed;
+  }
+
+  & > a:hover {
+    color: steelblue;
+  }
+`;
